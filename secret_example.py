d1311adedb9141a6b3519437d37ee1b847b4ecc3
callum-i <callum.iddon@stfc.ac.uk>
2017-07-28 09:41:41 +0100

    Add example for secret.py

---
+++ secret_example.py
@@ -0,0 +1,8 @@
+# Mag DB
+MAG_DBNAME = "magdb name as a string"
+MAG_HOST = "magdb host as a string"
+MAG_USER = "magdb username as a string"
+MAG_PASSWD = "maddb password as a string"
+
+# ldap
+LDAP_HOST = "ldap host as a string"
