53e8de6a7959be020892fe59691dac1b0435cfcd
cal-id <callum.iddon@stfc.ac.uk>
2017-08-09 13:57:44 +0000

    Improve updateAll.py: python version + ALL
    Check for python version cleverly rather than forcing 2.7
    - useful for virtual environments where python2.7 == python
    
    Include Capacity and Pledges in updateAll... They do update,
    just at a slower frequency, ideally another script with another
    name can be used to update just those with a high frequency of
    changes.
    This fixes #8

--- updateAll.sh
+++ updateAll.sh
@@ -1,9 +1,16 @@
-# updateCapacity.py
-# updatePledges.py
-python2.7 updateDiskServersInIntervention.py
-python2.7 updateDowntimes.py
-python2.7 updateGgusTickets.py
-python2.7 updateNotices.py
-python2.7 updateStorageUsageDataSource_VO.py
-python2.7 updateStorageUsageDataSource_MoreDetails.py
-python2.7 updateGanglia.py
+python -c "import sys; assert sys.version_info > (2,7)"
+if [ $? -eq 1 ]
+then
+    echo "Use python2.7+"
+    exit 1
+fi
+
+updateCapacity.py
+updatePledges.py
+python updateDiskServersInIntervention.py
+python updateDowntimes.py
+python updateGgusTickets.py
+python updateNotices.py
+python updateStorageUsageDataSource_VO.py
+python updateStorageUsageDataSource_MoreDetails.py
+python updateGanglia.py
