f1d8069971004cf0eaccf6decaa35c5442f814f5
callum-i <callum.iddon@stfc.ac.uk>
2017-08-09 13:48:33 +0100

    Rewrite setupFolders.py to be easier to read

--- setupFolders.py
+++ setupFolders.py
@@ -1,101 +1,102 @@
+"""
+This script sets up the folders for some of the datasources:
+- Disk Servers In Intervention
+- Downtimes
+- GGUS Tickets
+- Notices
+- Storage Usage (this has an overall datasource AND one per VO)
+
+For each data source, it creates a top directory:
+    BASE_PATH + "something_to_identify_the_datasource"
+
+And a search file with in this top directory. This file is downloaded by
+Grafana to give it the titles of the data in this URL.
+"""
+
 from __future__ import print_function
 import os
+import sys
 
-# setup storage usage folders
+# INITIAL SETUP
 
-# store the path to place the folders in
-path = "/var/www/html/grafanaJsonDatasources/diskServersInIntervention"
+BASE_PATH = "/var/www/html/grafanaJsonDatasources/"
+assert BASE_PATH[-1] == "/"
 
-try:
-    os.makedirs(path)
-    print("Created folder: " + path)
-except OSError:
-    print("Folder already exists: " + path)
-try:
-    os.makedirs(path + "HTML")
-    print("Created folder: " + path + "HTML")
-except OSError:
-    print("Folder already exists: " + path + "HTML")
 
-# create this folder
+def makeDirectoryWithLog(path):
+    """Creates the folders (including parent folders) for a given path. This
+    should log if there was a success or failure."""
+    try:
+        os.makedirs(path, exist_ok=True)
+    except OSError as ex:
+        if sys.version_info < (3, 3) or isinstance(ex, PermissionError):
+            # In python3.3+ there is the concept of a Permission Error
+            print("Could not create {}. You don't have permission!"
+                  .format(path))
+    except BaseException as ex:
+        print("Failed to create {} with an unexpected error!".format(path))
+        raise
+    else:
+        print(path, "is available")
 
-# setup the search file: the search file is the one that is read
-# by grafana to create the dropdown box when selecting datasource
-with open(path + "/search", "w") as searchFile:
-    json = '["Disk Servers in Intervention"]'
-    searchFile.write(json)
-    print("Written file: " + path + "/search")
 
-with open(path + "HTML/search", "w") as searchFile:
-    json = '["Disk Servers in Intervention HTML"]'
-    searchFile.write(json)
-    print("Written file: " + path + "/search")
-from __future__ import print_function
-import os  # so that we can make folders
+def writeSearchFile(path, content):
+    """Writes content (string) to a new file 'search' located under path.
+    eg /var/www/html/grafanaJsonDatasources/diskServersInIntervention
+    writes content to
+    /var/www/html/grafanaJsonDatasources/diskServersInIntervention/search
+    """
+    filePath = os.path.join(path, "search")
+    try:
+        with open(filePath, "w") as searchFile:
+            searchFile.write(content)
+            print("Written file:", filePath)
+    except OSError as ex:
+        if sys.version_info < (3, 3) or isinstance(ex, PermissionError):
+            # In python3.3+ there is the concept of a Permission Error
+            print("Could not create {}. You don't have permission!"
+                  .format(filePath))
+    except BaseException as ex:
+        print("Failed to create {} with an unexpected error!".format(path))
+        raise
+    else:
+        print(path, "is available")
 
-path = "/var/www/html/grafanaJsonDatasources/downtimes"
 
-try:
-    os.makedirs(path)
-    print("Created folder at: " + path)
-except OSError:
-    print("Couldn't make folder, folder already exists: " + path)
+# DISK SERVERS IN INTERVENTION
+makeDirectoryWithLog(BASE_PATH + "diskServersInIntervention")
+makeDirectoryWithLog(BASE_PATH + "diskServersInInterventionHTML")
+writeSearchFile(BASE_PATH + "diskServersInIntervention",
+                '["Disk Servers in Intervention"]')
+writeSearchFile(BASE_PATH + "diskServersInInterventionHTML",
+                '["Disk Servers in Intervention HTML"]')
 
-with open(path + "/search", "w") as fh:
-    fh.write('["Downtimes"]')
-from __future__ import print_function
-import os  # so that we can make folders
 
-path = "/var/www/html/grafanaJsonDatasources/ggusTickets"
+# DOWNTIMES
+makeDirectoryWithLog(BASE_PATH + "downtimes")
+writeSearchFile(BASE_PATH + "downtimes", '["Downtimes"]')
 
-try:
-    os.makedirs(path)
-    print("Created folder at: " + path)
-except OSError:
-    print("Couldn't make folder, folder already exists: " + path)
+# GGUS TICKETS
+makeDirectoryWithLog(BASE_PATH + "ggusTickets")
+writeSearchFile(BASE_PATH + "ggusTickets", '["GGUS Tickets"]')
 
-with open(path + "/search", "w") as fh:
-    fh.write('["GGUS Tickets"]')
-from __future__ import print_function
-import os  # so that we can make folders
+# NOTICES
+makeDirectoryWithLog(BASE_PATH + "notices")
+writeSearchFile(BASE_PATH + "notices", '["Notices"]')
 
-path = "/var/www/html/grafanaJsonDatasources/notices"
-
-try:
-    os.makedirs(path)
-    print("Created folder at: " + path)
-except OSError:
-    print("Couldn't make folder, folder already exists: " + path)
-
-with open(path + "/search", "w") as fh:
-    fh.write('["Notices"]')
-from __future__ import print_function
-import os
-
-# setup storage usage folders
-
-vo_list = [
-    None, "alice", "atlas", "cms", "lhcb", "hone", "ilc", "mice", "minos",
-    "na62", "snoplus", "t2k", "superb", "dirac"
-]
+# STORAGE USAGE
+vo_list = [None, "alice", "atlas", "cms", "lhcb", "hone", "ilc", "mice",
+           "minos", "na62", "snoplus", "t2k", "superb", "dirac"]
 
 for vo in vo_list:
     # store the path to place the folders in
-    path = "/var/www/html/grafanaJsonDatasources/storageUsage"
-    path += "" if vo is None else vo.capitalize()
-    # if its the overall VO datasource then ad nothing to the path
+    thisPath = BASE_PATH + "storageUsage"
+    # if its the overall VO datasource then add nothing to the path
     # else add the vo's name but capitalized
-    try:
-        os.makedirs(path)
-        print("Created Folder: " + path)
-    except OSError:
-        print("Folder already exists: " + path)
-    # create this folder
-    # setup the search file: the search file is the one that is read
-    # by grafana to create the dropdown box when selecting datasource
-    with open(path + "/search", "w") as searchFile:
-        text = '["Storage Usage Feed '
-        text += "Overall" if vo is None else "For " + vo.capitalize()
-        text += '"]'
-        searchFile.write(text)
-        print("Written file: " + path + "/search")
+    thisPath += "" if vo is None else vo.capitalize()
+    makeDirectoryWithLog(thisPath)
+    # Create the search file content
+    searchFileContent = '["Storage Usage Feed '
+    searchFileContent += "Overall" if vo is None else "For " + vo.capitalize()
+    searchFileContent += '"]'
+    makeDirectoryWithLog(thisPath, searchFileContent)
