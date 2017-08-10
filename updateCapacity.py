3eed212120f574a8db717783a430322dba3a8873
callum-i <callum.iddon@stfc.ac.uk>
2017-08-10 11:00:20 +0100

    Import BASE_PATH in capacity

--- updateCapacity.py
+++ updateCapacity.py
@@ -14,6 +14,7 @@ import requests
 
 from utils import writeFileWithLog
 
+from config import BASE_PATH
 
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
