91a2f4625634b4180c63242a87e6759db82dccb6
callum-i <callum.iddon@stfc.ac.uk>
2017-08-25 15:46:59 +0100

    Catch all exceptions in setupFolders.py

--- setupFolders.py
+++ setupFolders.py
@@ -35,55 +35,58 @@ def writeSearchFile(path, content):
     writeFileWithLog(filePath, content)
 
 
-# DISK SERVERS IN INTERVENTION
-makeDirectoryWithLog(BASE_PATH + "diskServersInIntervention")
-makeDirectoryWithLog(BASE_PATH + "diskServersInInterventionHTML")
-writeSearchFile(BASE_PATH + "diskServersInIntervention",
-                '["Disk Servers in Intervention"]')
-writeSearchFile(BASE_PATH + "diskServersInInterventionHTML",
-                '["Disk Servers in Intervention HTML"]')
+try:
+    # DISK SERVERS IN INTERVENTION
+    makeDirectoryWithLog(BASE_PATH + "diskServersInIntervention")
+    makeDirectoryWithLog(BASE_PATH + "diskServersInInterventionHTML")
+    writeSearchFile(BASE_PATH + "diskServersInIntervention",
+                    '["Disk Servers in Intervention"]')
+    writeSearchFile(BASE_PATH + "diskServersInInterventionHTML",
+                    '["Disk Servers in Intervention HTML"]')
 
+    # DOWNTIMES
+    makeDirectoryWithLog(BASE_PATH + "downtimes")
+    writeSearchFile(BASE_PATH + "downtimes", '["Downtimes"]')
 
-# DOWNTIMES
-makeDirectoryWithLog(BASE_PATH + "downtimes")
-writeSearchFile(BASE_PATH + "downtimes", '["Downtimes"]')
+    # GGUS TICKETS
+    makeDirectoryWithLog(BASE_PATH + "ggusTickets")
+    writeSearchFile(BASE_PATH + "ggusTickets", '["GGUS Tickets"]')
 
-# GGUS TICKETS
-makeDirectoryWithLog(BASE_PATH + "ggusTickets")
-writeSearchFile(BASE_PATH + "ggusTickets", '["GGUS Tickets"]')
+    # NOTICES
+    makeDirectoryWithLog(BASE_PATH + "notices")
+    writeSearchFile(BASE_PATH + "notices", '["Notices"]')
 
-# NOTICES
-makeDirectoryWithLog(BASE_PATH + "notices")
-writeSearchFile(BASE_PATH + "notices", '["Notices"]')
+    # CAPACITY
+    for name in CAPACITY_DATA_LABELS:
+        makeDirectoryWithLog(BASE_PATH + "capacityOverTime" + name)
+        writeSearchFile(BASE_PATH + "capacityOverTime" + name,
+                        json.dumps([name]))
 
-# CAPACITY
-for name in CAPACITY_DATA_LABELS:
-    makeDirectoryWithLog(BASE_PATH + "capacityOverTime" + name)
-    writeSearchFile(BASE_PATH + "capacityOverTime" + name, json.dumps([name]))
+    # PLEDGES
+    for key in PLEDGES_ROW_DATA_LABELS:
+        makeDirectoryWithLog(BASE_PATH + "pledgesOverTime" + key)
+        writeSearchFile(BASE_PATH + "pledgesOverTime" + key,
+                        json.dumps(PLEDGES_EXPERIMENT_DATA_LABELS))
+        makeDirectoryWithLog(BASE_PATH + "pledgesOverTime" + key + "SumOnly")
+        writeSearchFile(BASE_PATH + "pledgesOverTime" + key + "SumOnly",
+                        json.dumps([PLEDGES_EXPERIMENT_DATA_LABELS[-1]]))
 
-# PLEDGES
-for key in PLEDGES_ROW_DATA_LABELS:
-    makeDirectoryWithLog(BASE_PATH + "pledgesOverTime" + key)
-    writeSearchFile(BASE_PATH + "pledgesOverTime" + key,
-                    json.dumps(PLEDGES_EXPERIMENT_DATA_LABELS))
-    makeDirectoryWithLog(BASE_PATH + "pledgesOverTime" + key + "SumOnly")
-    writeSearchFile(BASE_PATH + "pledgesOverTime" + key + "SumOnly",
-                    json.dumps([PLEDGES_EXPERIMENT_DATA_LABELS[-1]]))
+    # STORAGE USAGE
+    vo_list = [None, "alice", "atlas", "cms", "lhcb", "hone", "ilc", "mice",
+               "minos", "na62", "snoplus", "t2k", "superb", "dirac"]
 
-
-# STORAGE USAGE
-vo_list = [None, "alice", "atlas", "cms", "lhcb", "hone", "ilc", "mice",
-           "minos", "na62", "snoplus", "t2k", "superb", "dirac"]
-
-for vo in vo_list:
-    # store the path to place the folders in
-    thisPath = BASE_PATH + "storageUsage"
-    # if its the overall VO datasource then add nothing to the path
-    # else add the vo's name but capitalized
-    thisPath += "" if vo is None else vo.capitalize()
-    makeDirectoryWithLog(thisPath)
-    # Create the search file content
-    searchFileContent = '["Storage Usage Feed '
-    searchFileContent += "Overall" if vo is None else "For " + vo.capitalize()
-    searchFileContent += '"]'
-    writeSearchFile(thisPath, searchFileContent)
+    for vo in vo_list:
+        # store the path to place the folders in
+        thisPath = BASE_PATH + "storageUsage"
+        # if its the overall VO datasource then add nothing to the path
+        # else add the vo's name but capitalized
+        thisPath += "" if vo is None else vo.capitalize()
+        makeDirectoryWithLog(thisPath)
+        # Create the search file content
+        searchFileContent = '["Storage Usage Feed '
+        searchFileContent += ("Overall"
+                              if vo is None else "For " + vo.capitalize())
+        searchFileContent += '"]'
+        writeSearchFile(thisPath, searchFileContent)
+except BaseException as ex:
+    logger.exception(ex)
