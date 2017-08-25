a690c7e17eb9b767492cd1daf9a15f3bd8736038
callum-i <callum.iddon@stfc.ac.uk>
2017-08-25 10:39:57 +0100

    Format query string. Use config for spaceToken.
    Tidy up updateDiskServersInIntervention.py
    * Use .format() to create the query string rather than ""+""
    * import SPACE_TOKEN_MAP from config
    * Improve comment readbility

--- updateDiskServersInIntervention.py
+++ updateDiskServersInIntervention.py
@@ -1,134 +1,53 @@
 from __future__ import print_function
 from pg import DB  # from PyGreSQL import database function
 from secret import MAG_DBNAME, MAG_HOST, MAG_USER, MAG_PASSWD
-from config import BASE_PATH, URL_OVERWATCH_MACHINE_NAME
-import json
-# get the json module so that the object can be serialized at the end
-
+from config import BASE_PATH, URL_OVERWATCH_MACHINE_NAME, SPACE_TOKEN_MAP
+import json  # For formatting the output
 from utils import writeFileWithLog, createHTMLLinkString
 
-db = DB(dbname=MAG_DBNAME, host=MAG_HOST, user=MAG_USER, passwd=MAG_PASSWD)
 # log into the database as the object db
+db = DB(dbname=MAG_DBNAME, host=MAG_HOST, user=MAG_USER, passwd=MAG_PASSWD)
 
-listOfSelectValues = ["machineName", "virtualOrganisation", "diskPool", "dxtx"]
-# list of data to get from the database about each diskserver in intevention.
+# List of data to get from the database about each diskserver in intevention.
 # For now this is also a list of column headings
-'''
-List of possible items to select for each diskserver.
-At the moment, I am just going with the ones that Tiju had.
+listOfSelectValues = ["machineName", "virtualOrganisation", "diskPool", "dxtx"]
 
-virtualOrganisation
-diskPool
-serviceType
-numberFileSystems
-machineName
-sizeTb
-miscComments
-currentTeam
-hardwareGroup
-castorInstance
-lastVerified
-currentStatus
-dxtx
-normalStatus
-puppetManaged
-quattorManaged
-'''
+# Throw an error if there are no select values
+assert len(listOfSelectValues) > 0
 
-assert len(listOfSelectValues) != 0
-# throw an error if there are no select values
-# set up the begining of the query string (before adding the select values)
-queryString = 'SELECT '
-
-queryString += '"' + listOfSelectValues[0] + '"'  # add the first of the select
-# values (already threw an error if this doesn't exist)
-
-for selectValue in listOfSelectValues:
-    # add the rest of the select values with a comma for separation
-    # (if there even are any)
-    queryString += ', "' + selectValue + '"'
-
-# finish the end of the query string
-queryString += (' from "vCastor" '
-                'where ("virtualOrganisation" != \'n/a\' '
-                'and "virtualOrganisation" != \'vcert2\' '
-                'and "currentStatus" != \'Production\' '
-                'and  "currentStatus" != \'Draining\' '
-                'and  "currentStatus" != \'ReadOnly\') '
-                'and ("normalStatus" = \'Production\' '
-                'OR "normalStatus" = \'ReadOnly\') '
-                'order by "virtualOrganisation"')
-# example of a valid query string:
-# 'SELECT "machineName", "virtualOrganisation", "diskPool",  "dxtx"
-# from "vCastor"
-# where ("virtualOrganisation" != \'n/a\'
-# and "virtualOrganisation" != \'vcert2\'
-# and "currentStatus" != \'Production\'
-# and  "currentStatus" != \'Draining\'
-# and  "currentStatus" != \'ReadOnly\')
-# and ("normalStatus" = \'Production\'
-# OR "normalStatus" = \'ReadOnly\')
-# order by "virtualOrganisation"'
+queryString = ('SELECT {0} '
+               'from "vCastor" '
+               'where ("virtualOrganisation" != \'n/a\' '
+               'and "virtualOrganisation" != \'vcert2\' '
+               'and "currentStatus" != \'Production\' '
+               'and  "currentStatus" != \'Draining\' '
+               'and  "currentStatus" != \'ReadOnly\') '
+               'and ("normalStatus" = \'Production\' '
+               'OR "normalStatus" = \'ReadOnly\') '
+               'order by "virtualOrganisation"'
+               ).format('"' + '", "'.join(listOfSelectValues) + '"')
 
 # get a query object
 query = db.query(queryString)
 
-listOfResults = query.dictresult()
 # this is a list of dictionaries which have keys defined by the select values
 # above
 # an example of a return (only one item in list):
 # [{'virtualOrganisation': 'Facilities', 'machineName': 'fdsdss36', 'dxtx':
 # 'd1t0', 'diskPool': 'cedaDiskTest'}]
+listOfResults = query.dictresult()
 
-jsonObj = [{"columns": [], "rows": [], "type": "table"}]
 # the object which will eventually be converted into JSON
+jsonObj = [{"columns": [], "rows": [], "type": "table"}]
 
-jsonObj[0]["columns"] = [{"text": key} for key in listOfSelectValues]
 # set the predefined column based on the select values for the moment
+jsonObj[0]["columns"] = [{"text": key} for key in listOfSelectValues]
 
 if "diskPool" in listOfSelectValues:
     # if there is a diskpool column then we can add a Space Token column after
     # the diskpool
-    jsonObj[0]["columns"].insert(
-        listOfSelectValues.index("diskPool") + 1, {"text": "Space Token"})
-
-# a mapping from disk pool to machine name
-# so spacetoken = spaceTokenMap[diskPool]
-# this comes straight out of Tiju's code but translated into python dictionary
-spaceTokenMap = {
-    "atlasFarm": "",
-    "atlasHotDisk": "",
-    "atlasNonProd": "",
-    "atlasScratchDisk": "ATLASSCRATCHDISK",
-    "atlasSimRaw": "ATLASMCTAPE",
-    "atlasSimStrip": "ATLASMCDISK",
-    "atlasSpare": "",
-    "atlasStripDeg": "ATLASGROUPDISK",
-    "atlasStripInput": "ATLASDATADISK",
-    "atlasT0Raw": "ATLASDATATAPE",
-    "atlasTape": "ATLASDATATAPE",
-    "cmsFarmRead": "cmsFarmRead",
-    "cmsSpare": "",
-    "cmsTest": "",
-    "cmsDisk": "",
-    "cmsTape": "",
-    "cmsWanIn": "CMS_DEFAULT",
-    "cmsWanOut": "cmsWanOut",
-    "lhcbDst": "LHCb_DST",
-    "lhcbMdst": "LHCb_M-DST",
-    "lhcbNonProd": "",
-    "lhcbRawRdst": "LHCb_RAW",
-    "lhcbSpare": "",
-    "lhcbUser": "",
-    "aliceTape": "",
-    "dteamTest": "",
-    "genNonProd": "",
-    "genTape": "",
-    "genTest": "",
-    "xrootd": "xrootd",
-    "NFS": "NFS",
-    "upgrade": ""
-}
+    jsonObj[0]["columns"].insert(listOfSelectValues.index("diskPool") + 1,
+                                 {"text": "Space Token"})
 
 for item in listOfResults:
     thisRow = []
@@ -148,7 +67,7 @@ for item in listOfResults:
             # if we are in the diskPool column then we need to add a column for
             # space token
             try:
-                thisRow.append(spaceTokenMap[toAppend])
+                thisRow.append(SPACE_TOKEN_MAP[toAppend])
             except KeyError:
                 # if there isnt a mapping this is probably a problem so log
                 # this
